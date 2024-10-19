653a28683f134ad8af1a9f06611d3bbf7ab15551
DESKTOP-R0PJKEE\wjg <wjg980807@gmail.com>
2024-10-20 01:40:55 +0900

    Add postgres

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import TestView
+from django.contrib import admin
+from django.urls import include, path, re_path
+from django.conf import settings
+from rest_framework import routers
+from rest_framework import permissions
+
+
+
--- api/views.py
+++ api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
